255c127aa2c919d264c37c86e9b40fb4746a70c9
25023386-collab <25023386@vnu.edu.vn>
2026-02-28 22:49:01 +0700

    Xóa function
    Refactor Gcd method to print the result directly.

--- Javacode/Week1/src/gcd.java
+++ Javacode/Week1/src/gcd.java
@@ -6,18 +6,17 @@ public class gcd {
         int a = data.nextInt();
         System.out.print("Nhập số b: ");
         int b = data.nextInt();
-        System.out.print(Gcd(a,b));
-    }
-    public static int Gcd(int a, int b) {
         int max = 0;
         int temp = 0;
-        if (a>b) {
-            temp = a; a = b; b = temp;
+        if (a > b) {
+            temp = a;
+            a = b;
+            b = temp;
         }
-        for (int i = 1;i <= a; i++) {
-            if (a%i == 0 && b%i == 0)
+        for (int i = 1; i <= a; i++) {
+            if (a % i == 0 && b % i == 0)
                 max = i;
         }
-        return max;
+        System.out.print(max);
     }
 }
