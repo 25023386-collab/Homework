ff0c4fbeadfd8e384892b50cbf74e2a791337030
25023386-collab <25023386@vnu.edu.vn>
2026-02-28 22:50:07 +0700

    Refactor isPalindrome class to isPrime

--- Javacode/Week1/src/isPalindrome.java
+++ Javacode/Week1/src/isPalindrome.java
@@ -1,20 +1,18 @@
 import java.util.Scanner;
-public class isPalindrome {
+public class isPrime {
     public static void main(String[] args) {
         Scanner data = new Scanner(System.in);
         System.out.print("Nhập số n: ");
         int n = data.nextInt();
-        System.out.print(IsPalindrome(n));
-    }
-    public static boolean IsPalindrome(int x) {
-        int rev = 0;
-        int original = x;
-        while (x>0) {
-            rev = rev*10 + x%10;
-            x = x/10;
+        boolean check = true;
+        if (n < 2) {
+            check = false;
         }
-        if (original == rev) {return true;}
-        else {return false;}
+        for (int i = 2; i <= Math.sqrt(n); i++) {
+            if (n % i == 0) {
+                check = false;
+            }
+        }
+        System.out.print(check);
     }
-
 }
