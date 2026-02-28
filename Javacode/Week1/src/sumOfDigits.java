983efe17308cde0e302677a4a257b1636bda7958
25023386-collab <25023386@vnu.edu.vn>
2026-02-28 22:53:47 +0700

    Refactor sumOfDigits to print sum directly

--- Javacode/Week1/src/sumOfDigits.java
+++ Javacode/Week1/src/sumOfDigits.java
@@ -4,16 +4,13 @@ public class sumOfDigits {
     public static void main(String[] args) {
         Scanner data = new Scanner(System.in);
         System.out.print("Nhập số n: ");
-        int n = data.nextInt();
-        System.out.print(SumOfDigits(n));
-    }
-    public static int SumOfDigits(int x) {
+        int x = data.nextInt();
         int sum = 0;
         if (x < 0) {x=-x;}
         while (x > 0) {
             sum += x % 10;
             x = x / 10;
         }
-        return sum;
+        System.out.print(sum);
     }
 }
